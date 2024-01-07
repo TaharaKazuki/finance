e2ac346538e17a7744db6c47a03b299d7a00088e
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-01-07 21:51:12 +0900

    update

--- pages/index.vue
+++ pages/index.vue
@@ -11,15 +11,15 @@
     <Trend
       color="green"
       title="Income"
-      :amount="4000"
-      :last-amount="3000"
+      :amount="incomeTotal"
+      :last-amount="4100"
       :loading="pending"
     />
     <Trend
       color="red"
       title="Expense"
-      :amount="4000"
-      :last-amount="5000"
+      :amount="expenseTotal"
+      :last-amount="3800"
       :loading="pending"
     />
     <Trend
@@ -37,6 +37,24 @@
       :loading="pending"
     />
   </section>
+  <section class="flex justify-between mb-10">
+    <div>
+      <h2 class="text-2xl font-extrabold">Transactions</h2>
+      <div class="text-gray-500 dark:text-gray-400">
+        You have {{ incomeCount }} incomes and {{ expenseCount }} expenses this
+        period
+      </div>
+    </div>
+    <div>
+      <UButton
+        icon="i-heroicons-plus-circle"
+        color="white"
+        variant="solid"
+        label="Add"
+      />
+    </div>
+  </section>
+
   <section v-if="!pending">
     <div
       v-for="(transactionsOnDay, date) in transactionsGroupedByDate"
@@ -65,6 +83,24 @@ const selectedView = ref(transactionViewOptions[1])
 
 const { fetchTransactions } = useTransactions()
 const { transactions, pending } = await fetchTransactions()
+
+const income = computed(
+  () => transactions.value?.filter((t) => t.type === 'Income')
+)
+const expense = computed(
+  () => transactions.value?.filter((t) => t.type === 'Expense')
+)
+
+const incomeCount = computed(() => income.value?.length)
+const expenseCount = computed(() => expense.value?.length)
+
+const incomeTotal = computed(
+  () => income.value?.reduce((sum, transaction) => sum + transaction.amount!, 0)
+)
+const expenseTotal = computed(
+  () =>
+    expense.value?.reduce((sum, transaction) => sum + transaction.amount!, 0)
+)
 
 const refreshTransactions = async () => {
   const result = await fetchTransactions()
