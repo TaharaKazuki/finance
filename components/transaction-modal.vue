8a217ad58d010ff76919d32c98a0bf6400a7f603
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-01-08 11:28:24 +0900

    update

---
+++ components/transaction-modal.vue
@@ -0,0 +1,17 @@
+<template>
+  <UModal v-model="isModalOpen">
+    <UCard>
+      <template #header> Add Transaction </template>
+      <div>Hello!</div>
+    </UCard>
+  </UModal>
+</template>
+<script setup lang="ts">
+const props = defineProps<{ modelValue: boolean }>()
+const emit = defineEmits(['update:modelValue'])
+
+const isModalOpen = computed({
+  get: () => props.modelValue,
+  set: (value) => emit('update:modelValue', value),
+})
+</script>
